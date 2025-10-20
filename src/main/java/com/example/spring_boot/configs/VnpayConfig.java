518f7f7efc97ebae8b42ca19a837a68277e3c980
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2025-10-21 03:08:19 +0700

    update vnpayconfig

--- src/main/java/com/example/spring_boot/configs/VnpayConfig.java
+++ src/main/java/com/example/spring_boot/configs/VnpayConfig.java
@@ -11,6 +11,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
+
 @Component
 public class VNPayConfig {
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
